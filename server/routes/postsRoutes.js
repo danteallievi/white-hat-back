82ac8328e42f8a2a41831bbd1f816522cf0fc069
Dante Allievi <danteallievi@gmail.com>
2021-12-18 12:48:20 +0100

    Router to create a post added

--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
-const { getPosts, getPostById } = require("../controllers/postControllers");
+const {
+  getPosts,
+  getPostById,
+  createPost,
+} = require("../controllers/postControllers");
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.get("/:id", getPostById);
+router.post("/", createPost);
 
 module.exports = router;
