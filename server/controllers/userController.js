9dd8281517e83d14a46ff430e617ef2e26b650e2
osu <joanosuna9@gmail.com>
2021-12-18 13:54:46 +0100

    Added favoriteUserVideo function to userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../../database/models/user");
+const Post = require("../../database/models/post");
 
 const createUser = async (req, res, next) => {
   const { email, name } = req.body;
@@ -33,4 +34,33 @@ const getUser = async (req, res, next) => {
   }
 };
 
-module.exports = { createUser, getUser };
+const favoriteUserVideo = async (req, res, next) => {
+  const { userId } = req.params;
+  const { postId, type } = req.body;
+  try {
+    const user = await User.findById(userId);
+    const post = await Post.findById(postId);
+    if (!user && !post) {
+      const error = new Error("User or post not found");
+      error.code = 404;
+      return next(error);
+    }
+    if (type === "ADD") {
+      await User.findByIdAndUpdate(userId, {
+        $push: { favourites: postId },
+      });
+      await Post.findByIdAndUpdate(
+        { _id: postId },
+        { $push: { favourites: userId } }
+      );
+      return res.json();
+    }
+    await User.findByIdAndUpdate(userId, { $pull: { favourites: postId } });
+    await Post.findByIdAndUpdate(postId, { $pull: { favourites: userId } });
+    return res.json();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createUser, getUser, favoriteUserVideo };
